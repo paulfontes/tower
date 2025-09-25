fc66bc7bab916741d1ab87fe941ec5c049ae9067
Paul Fontes <pgfon22@gmail.com>
2025-09-25 15:56:43 -0600

    delete ticket works

--- client/src/pages/AccountPage.vue
+++ client/src/pages/AccountPage.vue
@@ -1,9 +1,44 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
 import { AppState } from '../AppState.js';
+import { Pop } from '@/utils/Pop.js';
+import { logger } from '@/utils/Logger.js';
+import { ticketsService } from '@/services/TicketsService.js';
+import UpcomingEvents from '@/components/UpcomingEvents.vue';
+
 
 const account = computed(() => AppState.account)
+const ticketEvents = computed(() => AppState.ticketEvents)
 
+onMounted(() => getMyEvents())
+
+
+async function getMyEvents() {
+  try {
+    await ticketsService.getMyTicketEvents()
+  }
+  catch (error) {
+    Pop.error(error);
+    logger.log('Cant get your events!', error)
+  }
+}
+
+async function deleteTicket(ticketId) {
+  const confirmed = await Pop.confirm('Are you sure you no longer want this ticket?')
+
+  if (!confirmed) {
+    return
+  }
+
+
+  try {
+    await ticketsService.deleteTicket(ticketId)
+  }
+  catch (error) {
+    Pop.error(error);
+    logger.log('ticket could not delete', error)
+  }
+}
 </script>
 
 <template>
@@ -12,6 +47,16 @@ const account = computed(() => AppState.account)
       <h1>Welcome {{ account.name }}</h1>
       <img class="rounded" :src="account.picture" alt="" />
       <p>{{ account.email }}</p>
+      <div class="container-fluid">
+        <section class="row">
+          <div v-for="ticket in ticketEvents" :key="ticket.id" class="col-4 d-flex justify-content-center g-2">
+            <UpcomingEvents :event="ticket.event" />
+            <div>
+              <button @click="deleteTicket(ticket.id)">Leave</button>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
     <div v-else>
       <h1>Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
--- client/src/services/TicketsService.js
+++ client/src/services/TicketsService.js
@@ -1,9 +1,20 @@
 import { AppState } from "@/AppState.js"
 import { api } from "./AxiosService.js"
-import { TicketProfile } from "@/models/Ticket.js"
+import { TicketEvent, TicketProfile } from "@/models/Ticket.js"
 import { logger } from "@/utils/Logger.js"
 
 class TicketsService {
+    async deleteTicket(ticketId) {
+        const response = await api.delete(`api/tickets/${ticketId}`)
+        logger.log('Deleted Ticket!', response.data)
+        const index = AppState.ticketEvents.findIndex(ticket => ticket.id == ticketId)
+        AppState.ticketEvents.splice(index, 1)
+    }
+    async getMyTicketEvents() {
+        const response = await api.get('account/tickets')
+        const ticketEvents = response.data.map(t => new TicketEvent(t))
+        AppState.ticketEvents = ticketEvents
+    }
     async createTicket(ticketData) {
         const response = await api.post('api/tickets', ticketData)
         const ticket = new TicketProfile(response.data)
